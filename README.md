eedc81817fd3ccfd7f183ba14cc7c5954796ce14
Larissa Nascimento <larissa.nascc15@gmail.com>
2022-03-26 22:44:42 -0300

    add readme

---
+++ README.md
@@ -0,0 +1,18 @@
+# Landingpage Coffe ☕
+
+### ✏️ Desafio trilha Discover ->  Construir uma homepage para uma marca de café, nas versões desktop e mobile.
+
+---
+
+## Desktop 
+![imagem desktop](./img/desktop.gif)
+
+---
+
+## Mobile
+
+![imagem desktop](./img/mobile.gif)
+
+---
+
+<center> Feito com ❤️ por @lnasccc </center>
--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const btnMobile = document.getElementById('btn-mobile')
 
 function toggleMenu() {
@@ -7,24 +6,3 @@ function toggleMenu() {
 }
 
 btnMobile.addEventListener('click', toggleMenu)
-=======
-const button = document.querySelector('#button')
-const navigation = document.querySelector('.navigation')
-
-button.addEventListener('click', open)
-button.addEventListener('click', close)
-
-function open() {
-  navigation.classList.add('active')
-  button.classList.remove('menuOpen')
-  button.classList.add('menuClose') 
-}
-
-
-function close() {
-  navigation.classList.remove('active')
-  button.classList.remove('menuClose')
-  button.classList.add('menuOpen')
-}
-
->>>>>>> e35f0bbd30dbba3bcad330e11ef45957a656e8e7
